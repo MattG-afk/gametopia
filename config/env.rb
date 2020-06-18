dfcd0828f07aab99f7dd8a83a49c6937f7d55355
github username <github email address>
2020-06-18 15:18:48 -0700

    fixed environment in  config

--- config/env.rb
+++ config/env.rb
@@ -1,12 +1,12 @@
 
 
-ENV["APP_ENV"] ||== "development"
+ENV['APP_ENV'] ||= "development"
 
 require "bundler/setup"
-Bundler.require{:default, ENV["APP_ENV"]}
+Bundler.require(:default, ENV['APP_ENV'])
 
 configure :development do
-    set :database, (adapter: 'sqlite3', database:'db/db.sqlite3')
+    set :database, {adapter: 'sqlite3', database:'db/db.sqlite3'}
 end
 
 require_all 'app'
