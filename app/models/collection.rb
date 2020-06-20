26478f2b04dfda380f723ddae7cc3535a2f488a9
github username <github email address>
2020-06-20 14:00:14 -0700

    edited erb and user to have email

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,5 +1,3 @@
 class Collection < ActiveRecord::Base
     belongs_to :user
-
-    validate
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
     has_secure_password
+
+    validates :password, :presence => true
+
+    has_many :games
 end
