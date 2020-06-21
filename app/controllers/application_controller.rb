592b2d380286af8c43ba3cf8e6d494c5f96d6d31
github username <github email address>
2020-06-20 22:16:14 -0700

    adjusted tags added logic to get /

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,9 @@ class ApplicationController < Sinatra::Base
     end
 
     get '/' do
+        if logged_in?
+            redirect "/users/#{current_user.id}"
+        else
         erb :home
     end
 
